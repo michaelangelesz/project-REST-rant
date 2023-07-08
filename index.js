b730d6436728d8859abe8b562da49cde486ae38a
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-07 21:30:38 -0400

    Add home page view

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.render('Home')
 })
 
 // 404 not found page
