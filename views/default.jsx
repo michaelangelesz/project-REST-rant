4ddd4f1ac6649bd2acd723820f7d8abffb711749
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-09 12:19:15 -0400

    Adding styles to REST-Rant

--- views/default.jsx
+++ views/default.jsx
@@ -1,16 +1,21 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
-    )
-  }  
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+          integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+          crossOrigin="anonymous"
+        ></link>
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+      <body>{html.children}</body>
+    </html>
+  );
+}
 
-module.exports = Def
+module.exports = Def;
--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,17 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }
-  
+function home() {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+      </main>
+      <a href="/places">
+        <button className="btn-primary">Places Page</button>
+      </a>
+    </Def>
+  );
+}
 
-module.exports = home
+module.exports = home;
