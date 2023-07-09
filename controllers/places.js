9937118d2dde45b8fde9e4a9a0772f77843bbc19
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-09 18:09:52 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,36 +1,29 @@
 const router = require("express").Router();
+const places = require("../models/places");
+
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
+});
 
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
-router.get("/", (req, res) => {
-  let places = [
-    {
-      id: 0,
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "images/spencer-chow-PF_zcUW_NYU-unsplash.jpg",
-      // https://unsplash.com/photos/PF_zcUW_NYU
-    },
-    {
-      id: 1,
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "images/allison-christine-nKzZTrJeM18-unsplash.jpg",
-      // https://unsplash.com/photos/nKzZTrJeM18
-    },
-  ];
-  res.render("places/index", { places });
-});
-
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = '/images/paish-zaini--9UJTnXpUXM-unsplash.jpg'
+    // https://unsplash.com/photos/-9UJTnXpUXM
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('./places')
 })
 
 module.exports = router;
---
+++ models/places.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+      id: 0,
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "images/spencer-chow-PF_zcUW_NYU-unsplash.jpg",
+      // https://unsplash.com/photos/PF_zcUW_NYU
+    },
+    {
+      id: 1,
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "images/allison-christine-nKzZTrJeM18-unsplash.jpg",
+      // https://unsplash.com/photos/nKzZTrJeM18
+    },
+  ];
