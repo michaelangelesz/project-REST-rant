ab34ba555212db33090d25cd46a8d1a49239b6df
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-09 11:39:45 -0400

    fix "key" prop warning
    In server terminal, while the front end looks fine:
    Warning: Each child in a list should have a unique "key" prop.
    
    Check the top-level render call using <main>. See https://reactjs.org/link/warning-keys for more information.
        at div
        in index

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ Node Modules: method-override, dotenv, express-react-views
 | Method | Path | Purpose |
 |--------------:|:-------------:|:--------------|
 | GET | `/` | Home page |
-| GET | `/places` | Places index page |
-| POST | `/places` | Create new place |
+| GET | `/places` | Index page listing all places |
+| POST | `/places` | Create a new place |
 | GET | `/places/new` | Form page for creating a new place |
-| GET | `/places/:id` | Details about a particular place |
+| GET | `/places/:id` | Show one place in detail (Associated rants, new rant form, delete rant button) |
 | PUT | `/places/:id` | Update a particular place |
 | GET | `/places/:id/edit` | Form page for editing an existing place |
 | DELETE | `/places/:id` | Delete a particular place |
--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,25 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]      
-    res.render('places/index', { places })
-})
+router.get("/", (req, res) => {
+  let places = [
+    {
+      id: 0,
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "http://placekitten.com/250/250",
+    },
+    {
+      id: 1,
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "http://placekitten.com/250/250",
+    },
+  ];
+  res.render("places/index", { places });
+});
 
-module.exports = router
+module.exports = router;
