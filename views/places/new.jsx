6ea8f936947be7fa6936ca123dbc666d49606c33
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-10 11:40:53 -0400

    Make sure users provide a valid image link

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -13,7 +13,7 @@ function new_form() {
           </div>
           <div className="form-group">
             <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" id="pic" name="pic" />
+            <input className="form-control" id="pic" name="pic" pattern="https?://.+" />
           </div>
           <div className="form-group">
             <label htmlFor="city">City</label>
