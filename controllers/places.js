2e26337bb4c45eec487d10c7b59ccf94811943dc
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-13 13:29:19 -0400

    update navbar & footer, add icons & copyright date

--- controllers/places.js
+++ controllers/places.js
@@ -25,6 +25,33 @@ router.post('/', (req, res) => {
   res.redirect('./places')
 })
 
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,91 +1,105 @@
-const React = require("react");
-const Def = require("../default");
+const React = require("react")
+const Def = require("../default")
 
 function new_form() {
   return (
-    <Def title="Add a New Place" >
+    <Def title="Add a New Place">
       <main>
-      <head>
-        <link rel="icon" type="image/png" href="favicons/wine.png" />
-      </head>
+        <head>
+          <link rel="icon" type="image/png" href="favicons/wine.png" />
+        </head>
         <h1>Add a New Place</h1>
         <form method="POST" action="/places">
-          <div className="form-group">
-            <label htmlFor="name">Place Name</label>
-            <input className="form-control" id="name" name="name" required />
+          <div className="row">
+            <div className="form-group col-sm-6">
+              <label htmlFor="name">Place Name</label>
+              <input className="form-control" id="name" name="name" required />
+            </div>
+            <div className="form-group col-sm-6">
+              <label htmlFor="pic">Place Picture</label>
+              <input
+                className="form-control"
+                id="pic"
+                name="pic"
+                pattern="https?://.+"
+              />
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="pic">Place Picture</label>
-            <input className="form-control" id="pic" name="pic" pattern="https?://.+" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="city">City</label>
-            <input className="form-control" id="city" name="city" />
-          </div>
-          <div className="form-group">
-            <label htmlFor="state">State</label>
-            <select className="form-control" id="state" name="state">
-              <option value="AL">AL</option>
-              <option value="AK">AK</option>
-              <option value="AZ">AZ</option>
-              <option value="AR">AR</option>
-              <option value="CA">CA</option>
-              <option value="CO">CO</option>
-              <option value="CT">CT</option>
-              <option value="DE">DE</option>
-              <option value="FL">FL</option>
-              <option value="GA">GA</option>
-              <option value="HI">HI</option>
-              <option value="ID">ID</option>
-              <option value="IL">IL</option>
-              <option value="IN">IN</option>
-              <option value="IA">IA</option>
-              <option value="KS">KS</option>
-              <option value="KY">KY</option>
-              <option value="LA">LA</option>
-              <option value="ME">ME</option>
-              <option value="MD">MD</option>
-              <option value="MA">MA</option>
-              <option value="MI">MI</option>
-              <option value="MN">MN</option>
-              <option value="MS">MS</option>
-              <option value="MO">MO</option>
-              <option value="MT">MT</option>
-              <option value="NE">NE</option>
-              <option value="NV">NV</option>
-              <option value="NH">NH</option>
-              <option value="NJ">NJ</option>
-              <option value="NM">NM</option>
-              <option value="NY">NY</option>
-              <option value="NC">NC</option>
-              <option value="ND">ND</option>
-              <option value="OH">OH</option>
-              <option value="OK">OK</option>
-              <option value="OR">OR</option>
-              <option value="PA">PA</option>
-              <option value="RI">RI</option>
-              <option value="SC">SC</option>
-              <option value="SD">SD</option>
-              <option value="TN">TN</option>
-              <option value="TX">TX</option>
-              <option value="UT">UT</option>
-              <option value="VT">VT</option>
-              <option value="VA">VA</option>
-              <option value="WA">WA</option>
-              <option value="WV">WV</option>
-              <option value="WI">WI</option>
-              <option value="WY">WY</option>
-            </select>
+          <div className="row">
+            <div className="form-group col-sm-6">
+              <label htmlFor="city">City</label>
+              <input className="form-control" id="city" name="city" />
+            </div>
+            <div className="form-group col-sm-6">
+              <label htmlFor="state">State</label>
+              <select className="form-control" id="state" name="state">
+                <option value="AL">AL</option>
+                <option value="AK">AK</option>
+                <option value="AZ">AZ</option>
+                <option value="AR">AR</option>
+                <option value="CA">CA</option>
+                <option value="CO">CO</option>
+                <option value="CT">CT</option>
+                <option value="DE">DE</option>
+                <option value="FL">FL</option>
+                <option value="GA">GA</option>
+                <option value="HI">HI</option>
+                <option value="ID">ID</option>
+                <option value="IL">IL</option>
+                <option value="IN">IN</option>
+                <option value="IA">IA</option>
+                <option value="KS">KS</option>
+                <option value="KY">KY</option>
+                <option value="LA">LA</option>
+                <option value="ME">ME</option>
+                <option value="MD">MD</option>
+                <option value="MA">MA</option>
+                <option value="MI">MI</option>
+                <option value="MN">MN</option>
+                <option value="MS">MS</option>
+                <option value="MO">MO</option>
+                <option value="MT">MT</option>
+                <option value="NE">NE</option>
+                <option value="NV">NV</option>
+                <option value="NH">NH</option>
+                <option value="NJ">NJ</option>
+                <option value="NM">NM</option>
+                <option value="NY">NY</option>
+                <option value="NC">NC</option>
+                <option value="ND">ND</option>
+                <option value="OH">OH</option>
+                <option value="OK">OK</option>
+                <option value="OR">OR</option>
+                <option value="PA">PA</option>
+                <option value="RI">RI</option>
+                <option value="SC">SC</option>
+                <option value="SD">SD</option>
+                <option value="TN">TN</option>
+                <option value="TX">TX</option>
+                <option value="UT">UT</option>
+                <option value="VT">VT</option>
+                <option value="VA">VA</option>
+                <option value="WA">WA</option>
+                <option value="WV">WV</option>
+                <option value="WI">WI</option>
+                <option value="WY">WY</option>
+              </select>
+            </div>
           </div>
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
-            <input className="form-control" id="cuisines" name="cuisines" required />
+            <input
+              className="form-control"
+              id="cuisines"
+              name="cuisines"
+              required
+            />
           </div>
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
       </main>
     </Def>
-  );
+  )
 }
 
-module.exports = new_form;
+module.exports = new_form
