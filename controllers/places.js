07a519443a176c94b8b75ba928afdcb2b50b24da
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-12 22:45:43 -0400

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -38,6 +38,19 @@ router.get('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
