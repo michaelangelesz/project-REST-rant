8331f81fb53cfff533d431004605b1e09de308cb
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-07-26 16:42:59 -0400

    Add 'delete comment' ability

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -27,6 +27,9 @@ function show(data) {
             <stong>- {c.author}</stong>
           </h3>
           <h4>Rating: {c.stars}</h4>
+          <form method="POST" action={`/places/${data.place.id}/comment/${c.id}?_method=DELETE`}>
+          <input type="submit" className="btn btn-danger" value="Delete Comment" />
+        </form>
         </div>
       )
     })
