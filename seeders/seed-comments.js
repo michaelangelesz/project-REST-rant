6c3f90d8e50e3779765c6ad4c4fd0131fb98d282
michaelangelesz <125311967+michaelangelesz@users.noreply.github.com>
2023-09-29 18:53:12 -0400

    update footer links, seed data

--- seeders/seed-comments.js
+++ seeders/seed-comments.js
@@ -18,8 +18,33 @@ async function seed() {
   // Add that comment to the place's comment array.
   place.comments.push(comment.id)
 
+  // Add different comment on separate place
+  let place2 = await db.Place.findOne({ name: "Grub Hut" })
+
+  let comment2 = await db.Comment.create(
+    {
+      author: "Chopstick Charles",
+      rant: false,
+      stars: 5.0,
+      content: "heck yea chopstick food",
+    }
+  )
+
+  let comment3 = await db.Comment.create(
+    {
+      author: "Smart Samsara",
+      rant: false,
+      stars: 5.0,
+      content: "I could tell from the description the food was going to be amazing",
+    }
+  )    
+
+  place2.comments.push(comment2.id)
+  place2.comments.push(comment3.id)
+  
   //save the place now that it has comment
   await place.save()
+  await place2.save()
 
   // Exit the program
   process.exit()
